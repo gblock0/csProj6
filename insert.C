b0b9f25d29a8b120fdef5b9969335ddaadceebc0
Greg Block <ggblock@wisc.edu>
2012-11-28 21:47:15 -0600

    Coded insert

--- insert.C
+++ insert.C
@@ -19,6 +19,50 @@ const Status QU_Insert(const string & relation,
 	const attrInfo attrList[])
 {
   Status status;
+  AttrDesc *attrs;
+  Record record;
+  RID outRid;
+  RelDesc rd;
+  int realCnt;
+  InsertFileScan* ifs = new InsertFileScan(relation, status);
+  if (status != OK) return status;
+
+
+  //Check to make sure there are attributes in the attrList
+  if(attrCnt == 0 || sizeof(attrList) == 0){
+    /* **************NEED TO CHANGE********** */
+    return status;
+  }
+
+
+  //Get attribute data
+  status = attrCat->getRelInfo(relation, realCnt, attrs);
+  if (status != OK) return status;
+
+
+  for(int i = 0; i < sizeof(attrs); i++){
+    for(int j = 0; j < sizeof(attrList); j++){
+      if(attrs[i].attrName == attrList[j].attrName){
+        
+        /* 
+        status = attrCat->getInfo(attrs[i].relName, attrs[i].attrName, attrOrdered[i]);
+        if (status != OK) return status;
+        */
+  
+        //Make the record of the attribute date
+        record.data = attrList[j].attrValue;
+        record.length = attrList[j].attrLen;
+
+        //insert record into relation
+        status = ifs->insertRecord(record, outRid);
+        if (status != OK) return status;
+        break;
+      }
+    }
+  }
+  
+
+
 
   return status;
 
