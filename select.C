4bafbb068835bd1c56740579cd21ac4f187cccef
Alex <ardetrick@wisc.edu>
2012-12-03 12:56:50 -0600

    working better, not great

--- select.C
+++ select.C
@@ -61,6 +61,10 @@ const Status QU_Select(const string & result,
     
   attrDesc = (AttrDesc*) malloc(sizeof(AttrDesc));
     
+    string bigTable(projNames[0].relName);
+    cout << bigTable << endl;
+    
+    
   //we need to get the attribute info for the result table 
     //so we add the correct attributes
   status = attrCat->getRelInfo(result, attrCnt, attrs);
@@ -80,18 +84,30 @@ const Status QU_Select(const string & result,
         reclen += attrs[j].attrLen;
         //this is slightly bootleg, because it just overwrites 
           memcpy(&(projNamesArray[i].relName), &(projNames[i].relName), sizeof(projNames[i].relName));
+          //set offset
+          
+          
+        }
       }
     }
-  }
 
-  //if attr is NULL we still need an attrDesc over so we have relation name
+  //if attr is NULL we still need an attrDesc over so we can use relName
   if(attr == NULL){
     memcpy(attrDesc, &(projNames[0]), sizeof(AttrDesc));
+      cout << "in NULL" << endl;
   } else {  
     //else we need to find the matching attrDesc to convert from info to desc
+      cout << "in loop" << endl;
     for(int i = 0; i < projCnt; i++){
       strcomp = strcmp(attr->attrName, projNamesArray[i].attrName);
+        
+        string s1(attr->attrName);
+        string s2(projNamesArray[i].attrName);
+        cout << s1 << endl;
+        cout << s2<<endl;
+        
       if(strcomp == 0){
+          cout << "found something in loop" << endl;
         memcpy(attrDesc, &(projNamesArray[i]), sizeof(AttrDesc));
       }
     }
@@ -124,19 +140,26 @@ const Status ScanSelect(const string & result,
   Record rec;
   Record newRec;
   RID rid;
-  //int tupleOffset;
   void *tuple;
   int attrCnt;
   AttrDesc *attrs;
+  float tempFloat;  
+  int tempInt;
+    int strcomp;
     
   //convert from C string to C++ String
   string strBTRelName(attrDesc->relName);
-    string goodName(projNames[0].relName);
     
-    cout << result << endl;
-  cout << goodName << endl;
+    /*string goodName(projNames[0].relName);
+  
+    cout << strBTRelName << endl;
+  cout << result << endl;
+  cout << goodName << endl;*/
     
   hfs = new HeapFileScan(strBTRelName, status);
+    cout << "here " << endl;
+    cout << strBTRelName << endl;
+    //ASSERT(false);
   if(status != OK) return status;
   
   ifs = new InsertFileScan(result, status);
@@ -146,31 +169,51 @@ const Status ScanSelect(const string & result,
   cout << "clear skys" << endl;
   //start scan seraching for the attrDesc that matches the filter and op
  
-    /*
-    if(filter != NULL)
-    {
+  if(filter != NULL)
+  {
     
-      cout << "SCAN THE WATERS" << endl;
-      cout << attrDesc->attrOffset << endl;
-      cout << attrDesc->attrLen << endl;
-      cout << (attrDesc->attrType) << endl;
-      string s5(filter);
-       cout << s5 << endl;
-        cout << op << endl;
-    }*/
+    cout << "SCAN THE WATERS" << endl;
+      cout << strBTRelName << endl;
+    cout << attrDesc->attrOffset << endl;
+    cout << attrDesc->attrLen << endl;
+    cout << (attrDesc->attrType) << endl;
+    string s5(filter);
+    cout << s5 << endl;
+    cout << op << endl;
+  }
     
-    //filter should be fine, op should be fine
-    
-  status = hfs->startScan(attrDesc->attrOffset,attrDesc->attrLen,(Datatype) attrDesc->attrType, filter, op);
-  if(status != OK){ 
-    cout << "bad weather " << endl; 
+  //check attr type and cast accordingly
+  if(attrDesc->attrType == INTEGER){
+    tempInt = atoi(filter);
+    filter = (char *) &tempInt;
+    cout << "in int" << endl;
+  } else if(attrDesc->attrType == FLOAT){
+        tempFloat = atof(filter);
+        filter = (char *) &tempFloat;
+        cout << "in float" << endl;
+  }
+   
+  status = attrCat->getRelInfo(strBTRelName, attrCnt, attrs);
+  if (status != OK){
+    cout << "broken mast " << endl; 
     return status;
   }
     
-  status = attrCat->getRelInfo(strBTRelName, attrCnt, attrs);
-  if (status != OK){
-    cout << attrDesc->relName[0] <<endl;
-    cout << "broken mast " << endl; 
+    
+    //boot leg fix for offset
+    int offsetTest;
+    for(int i = 0; i < attrCnt; i++){
+        strcomp = strcmp(attrs[i].attrName, attrDesc->attrName);
+        if (strcomp == 0) {
+            offsetTest = attrs[i].attrOffset;
+            //memcpy(&(attrDesc->attrOffset), (attrs[i].attrOffset), sizeof(attrDesc->attrOffset));
+            cout << "new offset: " << offsetTest <<endl;
+        }
+    }
+    
+  status = hfs->startScan( (const int) offsetTest,attrDesc->attrLen,(Datatype) attrDesc->attrType, filter, op);
+  if(status != OK){ 
+    cout << "bad weather " << endl; 
     return status;
   }
 
