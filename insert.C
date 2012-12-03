1cab62c12a5306995c7dc2b216f834e226ef4b35
Alex <ardetrick@wisc.edu>
2012-12-03 14:19:18 -0600

    still working on insert

--- insert.C
+++ insert.C
@@ -24,45 +24,94 @@ const Status QU_Insert(const string & relation,
   RID outRid;
   RelDesc rd;
   int realCnt;
-  InsertFileScan* ifs = new InsertFileScan(relation, status);
-  if (status != OK) return status;
+  InsertFileScan* ifs;
+  int strComp;
+  int tupleSize;
+  void *tuplePtr;
+  int tupleOffset;
+    void *tupleOffsetPtr;
 
-
+    cout << "in insert" << endl;
+    
+    ifs= new InsertFileScan(relation, status);
+    if(status != OK) return status;
+    
+    cout << "after ifs creation" << endl;
+    
   //Check to make sure there are attributes in the attrList
   if(attrCnt == 0 || sizeof(attrList) == 0){
-    /* **************NEED TO CHANGE********** */
-    return status;
+    /* **************NEED TO CHANGE to another error********** */
+    return FILEEOF;
   }
 
+    cout << "after check on attributes in attrList" << endl;
 
   //Get attribute data
   status = attrCat->getRelInfo(relation, realCnt, attrs);
   if (status != OK) return status;
 
-
-  for(int i = 0; i < sizeof(attrs); i++){
-    for(int j = 0; j < sizeof(attrList); j++){
-      if(attrs[i].attrName == attrList[j].attrName){
-        
-        /* 
-        status = attrCat->getInfo(attrs[i].relName, attrs[i].attrName, attrOrdered[i]);
-        if (status != OK) return status;
-        */
+    cout << "got rel info" << endl;
+    
+  //calculate size of a new tuple
+    tupleSize = 0;
+    for(int i= 0; i < realCnt; i++)
+    {
+        tupleSize += attrs[i].attrLen;
+    }
+    
+    cout << "after size calculation" << endl;
+    
+  tuplePtr = malloc(tupleSize);
+    //need to add malloc error status 
+    if(tuplePtr == NULL)ASSERT(false);
   
-        //Make the record of the attribute date
-        record.data = attrList[j].attrValue;
-        record.length = attrList[j].attrLen;
-
-        //insert record into relation
-        status = ifs->insertRecord(record, outRid);
-        if (status != OK) return status;
+    cout << "after malloc" << endl;
+    
+  tupleOffset = 0; 
+  for(int i = 0; i < realCnt; i++){
+    for(int j = 0; j < attrCnt; j++){
+      strComp = strcmp(attrs[i].attrName, attrList[j].attrName); 
+      cout << " i dont think your like this" << endl;
+      string s1(attrs[i].attrName);
+      cout << s1<< endl;
+      string s2(attrList[j].attrName);
+      cout << s2 << endl;
+      cout << attrList[j].attrLen << endl;
+        
+      if(strComp == 0){
+        //get offset
+        cout << "are you even here?" << endl;
+          
+        if(attrList[j].attrLen == NULL)
+        {
+          //need to make new return status
+          cout << "null value" << endl;
+          return FILEEOF;
+        }
+          
+        tupleOffsetPtr = (void*) (((char*) tuplePtr) + tupleOffset);
+        //copy data into new tuple
+          cout << attrList[j].attrLen << endl;
+        memcpy(tupleOffsetPtr, &(attrList[j].attrValue), attrList[j].attrLen);
+          cout << "dont be like this" << endl;
+        //add to offset
+        tupleOffset += attrList[j].attrLen;
         break;
       }
     }
   }
   
+    cout << "after tuple reation" << endl;
+    
+    record.data = tuplePtr;
+    record.length = tupleSize;
+    
+    status = ifs->insertRecord(record, outRid);
+    if (status != OK) return status;
 
-
+    free(tuplePtr);
+    delete ifs;
+    delete attrs;
 
   return status;
 
